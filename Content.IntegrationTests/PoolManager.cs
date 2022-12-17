58a43d3bd561a5c23b5b5bd73864373cc8898e01
lunar-crater-ex <112364074+lunar-crater-ex@users.noreply.github.com>
2022-12-17 09:16:13 -0600

    ez test fixe

--- Content.IntegrationTests/PoolManager.cs
+++ Content.IntegrationTests/PoolManager.cs
@@ -11,6 +11,7 @@ using Content.IntegrationTests.Tests.Destructible;
 using Content.IntegrationTests.Tests.DeviceNetwork;
 using Content.IntegrationTests.Tests.Interaction.Click;
 using Content.IntegrationTests.Tests.Networking;
+using Content.Server._00Citadel.Worldgen;
 using Content.Server.GameTicking;
 using Content.Shared.CCVar;
 using NUnit.Framework;
@@ -56,6 +57,7 @@ public static class PoolManager
         (CCVars.VelocityConstraintsPerThread.Name, "999", true),
         (CCVars.ThreadParallelCount.Name, "1", true),
         (CCVars.GameRoleTimers.Name, "false", false),
+        (WorldgenCVars.WorldgenEnabled.Name, "false", false), // CITADEL EDIT
     };
 
     private static int PairId;
