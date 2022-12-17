401a536850ead9eb5556b97965b0e06065d8b825
lunar-crater-ex <112364074+lunar-crater-ex@users.noreply.github.com>
2022-12-16 23:10:21 -0600

    i like my tests passing

--- Content.Client/Entry/EntryPoint.cs
+++ Content.Client/Entry/EntryPoint.cs
@@ -133,6 +133,12 @@ namespace Content.Client.Entry
             _prototypeManager.RegisterIgnore("nukeopsRole");
             _prototypeManager.RegisterIgnore("flavor");
 
+            // BEGIN CITADEL EDITS
+            _prototypeManager.RegisterIgnore("noiseChannel");
+            _prototypeManager.RegisterIgnore("worldgenConfig");
+            _prototypeManager.RegisterIgnore("biome");
+            // END CITADEL EDITS
+
             _componentFactory.GenerateNetIds();
             _adminManager.Initialize();
             _stylesheetManager.Initialize();
