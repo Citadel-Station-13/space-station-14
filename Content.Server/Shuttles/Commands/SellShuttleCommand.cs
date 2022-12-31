cf1e07d5e619ef1eb22abd81819ef335abfa01b3
Cheackraze <71046427+Cheackraze@users.noreply.github.com>
2022-12-31 04:49:11 -0500

    sell

---
+++ Content.Server/Shuttles/Commands/SellShuttleCommand.cs
@@ -0,0 +1,44 @@
+using Content.Server.Administration;
+using Content.Server.Shuttles.Systems;
+using Content.Shared.Administration;
+using Robust.Shared.Console;
+
+namespace Content.Server.Shuttles.Commands;
+
+/// <summary>
+/// sells a shuttle.
+/// </summary>
+[AdminCommand(AdminFlags.Fun)]
+public sealed class SellShuttleCommand : IConsoleCommand
+{
+    public string Command => "sellshuttle";
+    public string Description => "appraises and sells a selected grid connected to selected station";
+    public string Help => $"{Command}";
+    public void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        if (!EntityUid.TryParse(args[0], out var stationId))
+        {
+            shell.WriteError($"{args[0]} is not a valid entity uid.");
+            return;
+        }
+        if (!EntityUid.TryParse(args[1], out var shuttleId))
+        {
+            shell.WriteError($"{args[0]} is not a valid entity uid.");
+            return;
+        };
+        var system = IoCManager.Resolve<IEntitySystemManager>().GetEntitySystem<ShuttleSystem>();
+        system.SellShuttle(stationId, shuttleId);
+    }
+
+    public CompletionResult GetCompletion(IConsoleShell shell, string[] args)
+    {
+        switch (args.Length)
+        {
+            case 1:
+                return CompletionResult.FromHint(Loc.GetString("station-id"));
+            case 2:
+                return CompletionResult.FromHint(Loc.GetString("shuttle-id"));
+        }
+        return CompletionResult.Empty;
+    }
+}
--- Content.Server/Shuttles/Systems/ShipyardSystem.cs
+++ Content.Server/Shuttles/Systems/ShipyardSystem.cs
@@ -3,6 +3,7 @@ using Content.Server.Station.Components;
 using Content.Server.Cargo.Systems;
 using Robust.Server.Maps;
 using Robust.Shared.Map;
+using Content.Shared.MobState.Components;
 
 namespace Content.Server.Shuttles.Systems;
 
@@ -11,6 +12,7 @@ public sealed partial class ShuttleSystem
     public MapId? ShipyardMap { get; private set; }
 
     [Dependency] private readonly PricingSystem _pricing = default!;
+    [Dependency] private readonly CargoSystem _cargo = default!;
     private void InitializeShipyard()
     {
         SubscribeLocalEvent<StationDataComponent, ComponentInit>(OnShipyardStartup);
@@ -34,9 +36,10 @@ public sealed partial class ShuttleSystem
 
         var price = _pricing.AppraiseGrid(shuttle.Owner, null);
 
+        //can do FTLTravel later instead if we want to open that door
         TryFTLDock(shuttle, targetGrid.Value);
 
-        _sawmill.Warning($"Shuttle {shuttlePath} was purchased at {targetGrid} for {price} spacebucks");
+        _sawmill.Info($"Shuttle {shuttlePath} was purchased at {targetGrid} for {price} spacebucks");
     }
     /// <summary>
     /// loads a paused shuttle into the ShipyardMap from a file path
@@ -57,6 +60,51 @@ public sealed partial class ShuttleSystem
         _shuttleIndex += _mapManager.GetGrid(shuttle.Value).LocalAABB.Width + ShuttleSpawnBuffer;
 
         return (EntityUid) shuttle;
+    }
+    public void SellShuttle(EntityUid stationUid, EntityUid shuttleUid)
+    {
+        if (!TryComp<StationDataComponent>(stationUid, out var stationGrid) || !HasComp<ShuttleComponent>(shuttleUid) || !TryComp<TransformComponent>(shuttleUid, out var xform) || ShipyardMap == null) return;
+
+        var targetGrid = _station.GetLargestGrid(stationGrid);
+
+        if (targetGrid == null) return;
+
+        var gridDocks = GetDocks((EntityUid) targetGrid);
+        var shuttleDocks = GetDocks(shuttleUid);
+        var isDocked = false;
+
+        foreach (var shuttleDock in shuttleDocks)
+        {
+            foreach (var gridDock in gridDocks)
+            {
+                if (shuttleDock.DockedWith == gridDock.Owner)
+                {
+                    isDocked = true;
+                    break;
+                };
+            };
+            if (isDocked) break;
+        };
+
+        if (!isDocked)
+        {
+            _sawmill.Warning($"shuttle is not docked to that station");
+            return;
+        };
+
+        var mobQuery = GetEntityQuery<MobStateComponent>();
+        var xformQuery = GetEntityQuery<TransformComponent>();
+
+        if (_cargo.FoundOrganics(shuttleUid, mobQuery, xformQuery) == true)
+        {
+            _sawmill.Warning($"organics on board");
+            return;
+        };
+
+        //just yeet and delete for now. Might want to split it into another function later to send back to the shipyard map first to pause for something
+        var price = _pricing.AppraiseGrid(shuttleUid);
+        _mapManager.DeleteGrid(shuttleUid);
+        _sawmill.Info($"Sold shuttle {shuttleUid} for {price}");
     }
     private void CleanupShipyard()
     {
