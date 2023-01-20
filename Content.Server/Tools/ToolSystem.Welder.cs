6f963a87f68532fbc68a0181b9179f6d1b82e4bd
Chief-Engineer <119664036+Chief-Engineer@users.noreply.github.com>
2023-01-20 10:05:05 -0600

    Add welder toggle logs

--- Content.Server/Tools/ToolSystem.Welder.cs
+++ Content.Server/Tools/ToolSystem.Welder.cs
@@ -3,6 +3,7 @@ using Content.Server.Chemistry.Components;
 using Content.Server.Chemistry.Components.SolutionManager;
 using Content.Server.Chemistry.EntitySystems;
 using Content.Server.Tools.Components;
+using Content.Shared.Database;
 using Content.Shared.Examine;
 using Content.Shared.FixedPoint;
 using Content.Shared.Interaction;
@@ -108,6 +109,12 @@ namespace Content.Server.Tools
 
             welder.Lit = true;
 
+            // Logging
+            if (user != null)
+                _adminLogger.Add(LogType.InteractActivate, LogImpact.Low, $"{ToPrettyString(user.Value):user} toggled {ToPrettyString(uid):welder} on");
+            else
+                _adminLogger.Add(LogType.Action, LogImpact.Low, $"{ToPrettyString(uid):welder} toggled on");
+
             var ev = new WelderToggledEvent(true);
             RaiseLocalEvent(welder.Owner, ev, false);
 
@@ -144,6 +151,12 @@ namespace Content.Server.Tools
             Resolve(uid, ref item, ref light, ref appearance, false);
 
             welder.Lit = false;
+
+            // Logging
+            if (user != null)
+                _adminLogger.Add(LogType.InteractActivate, LogImpact.Low, $"{ToPrettyString(user.Value):user} toggled {ToPrettyString(uid):welder} off");
+            else
+                _adminLogger.Add(LogType.Action, LogImpact.Low, $"{ToPrettyString(uid):welder} toggled off");
 
             var ev = new WelderToggledEvent(false);
             RaiseLocalEvent(welder.Owner, ev, false);
@@ -204,6 +217,13 @@ namespace Content.Server.Tools
         private void OnWelderActivate(EntityUid uid, WelderComponent welder, ActivateInWorldEvent args)
         {
             args.Handled = TryToggleWelder(uid, args.User, welder);
+<<<<<<< HEAD
+=======
+            if (args.Handled)
+                args.WasLogged = true;
+            var hotEvent = new IsHotEvent() {IsHot = true};
+            RaiseLocalEvent(uid, hotEvent);
+>>>>>>> d6228d7a4 (Add welder toggle logs (#13303))
         }
 
         private void OnWelderAfterInteract(EntityUid uid, WelderComponent welder, AfterInteractEvent args)
