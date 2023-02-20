b72e1df0c43b4b9f08dbe2e069b7bbfc77ead683
lunar-crater-ex <112364074+lunar-crater-ex@users.noreply.github.com>
2023-02-20 16:32:23 -0600

    Improved merge tool.

--- Tools/upstream_merge_tool.ps1
+++ Tools/upstream_merge_tool.ps1
@@ -1,6 +1,6 @@
 ﻿Write-Output "Moony's upstream merge workflow tool."
 Write-Output "This tool can be stopped at any time, i.e. to finish a merge or resolve conflicts. Simply rerun the tool after having resolved the merge with normal git cli."
-Write-Output "Pay attention to any output from git!"
+Write-Output "Pay attention to any output from git! DO NOT RUN THIS ON A WORKING TREE WITH UNCOMMITTED FILES OF ANY KIND."
 $target = Read-Host "Enter the branch you're syncing toward (typically upstream/master or similar)"
 $refs = git log --reverse --format=format:%H HEAD.. $target
 
@@ -15,6 +15,16 @@ foreach ($unmerged in $refs) {
 
     if ($summary -ieq "automatic changelog update") {
         Write-Output ("Deliberately skipping changelog bot commit {0}." -f $unmerged)
+        Write-Output "== GIT (CONFLICTS ARE OKAY) =="
+        git merge --no-ff --no-commit --no-verify $unmerged
+        # DELIBERATELY IGNORE merge conflict markers. We're just going to undo the commit!
+        git add *
+        git commit -m ("squash! Merge tool skipping '{0}'" -f $unmerged)
+        $newhead = git log -n 1 --format=format:%H
+        git reset HEAD~ --hard
+        git reset $newhead --soft
+        git commit --amend --no-edit
+        Write-Output "== DONE =="
         continue
     }
 
@@ -30,9 +40,21 @@ foreach ($unmerged in $refs) {
         }
         1 {
             Write-Output "== GIT =="
-            git merge $unmerged
+            git merge --no-ff -m ("squash! Merge tool integrating '{0}'" -f $unmerged) $unmerged
             Write-Output "== DONE =="
         }
-        0 { Write-Output ("Skipping " -f $unmerged) }
+        0 {
+            Write-Output ("Skipping {0}" -f $unmerged)
+            Write-Output "== GIT (CONFLICTS ARE OKAY) =="
+            git merge --no-ff --no-commit --no-verify $unmerged
+            # DELIBERATELY IGNORE merge conflict markers. We're just going to undo the commit!
+            git add *
+            git commit -m ("squash! Merge tool skipping '{0}'" -f $unmerged)
+            $newhead = git log -n 1 --format=format:%H
+            git reset HEAD~ --hard
+            git reset $newhead --soft
+            git commit --amend --no-edit
+            Write-Output "== DONE =="
+        }
     }
 }
