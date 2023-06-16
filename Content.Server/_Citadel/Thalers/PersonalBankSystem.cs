6f8433d68ba01fe4a9eee916519e1208738516de
lunar-crater-ex <112364074+lunar-crater-ex@users.noreply.github.com>
2023-06-16 11:20:54 -0500

    we do a little banking

--- Content.Server/_Citadel/Thalers/PersonalBankSystem.cs
+++ Content.Server/_Citadel/Thalers/PersonalBankSystem.cs
@@ -1,9 +1,11 @@
-﻿using System.Linq;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using Content.Server._Citadel.Contracts;
 using Content.Server._Citadel.Contracts.Components;
 using Content.Server.Mind.Components;
 using Content.Server.Station.Systems;
 using Content.Shared.FixedPoint;
+using JetBrains.Annotations;
 
 namespace Content.Server._Citadel.Thalers;
 
@@ -23,11 +25,27 @@ public sealed class PersonalBankSystem : EntitySystem
         var contract = Comp<ContractComponent>(ev.Contract);
         foreach (var mind in contract.SubContractors.Append(contract.OwningContractor!))
         {
+            if (mind.OwnedEntity is null)
+                continue;
 
+            TryAdjustBalance(mind.OwnedEntity.Value, ev.Amount);
         }
     }
 
-    public bool TryAdjustBalance(EntityUid user, FixedPoint2 amount)
+    [PublicAPI]
+    public bool TryGetBalance(EntityUid user, [NotNullWhen(true)] out FixedPoint2? balance)
+    {
+        balance = null;
+
+        if (!TryComp<MindComponent>(user, out var mindComp) || mindComp.Mind is not { } mind || mind.BankAccount is not { } account)
+            return false;
+
+        balance = account.Thalers;
+        return true;
+    }
+
+    [PublicAPI]
+    public bool CanAdjustBalance(EntityUid user, FixedPoint2 amount)
     {
         if (!TryComp<MindComponent>(user, out var mindComp) || mindComp.Mind is not { } mind || mind.BankAccount is not { } account)
             return false;
@@ -41,7 +59,21 @@ public sealed class PersonalBankSystem : EntitySystem
         if (newSum < 0)
             return false;
 
-        account.Thalers = newSum;
+        return true;
+    }
+
+    [PublicAPI]
+    public bool TryAdjustBalance(EntityUid user, FixedPoint2 amount)
+    {
+        if (!TryComp<MindComponent>(user, out var mindComp) || mindComp.Mind is not { } mind || mind.BankAccount is not { } account)
+            return false;
+
+        if (!CanAdjustBalance(user, amount))
+            return false;
+
+        account.Thalers += amount;
+
+        return true;
     }
 }
 
