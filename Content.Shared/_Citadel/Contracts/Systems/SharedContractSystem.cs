fc424483bc5ff8f56f8f5cc9c6b1a8b9e9d0144e
lunar-crater-ex <112364074+lunar-crater-ex@users.noreply.github.com>
2024-07-04 18:42:04 -0500

    ae

--- Content.Shared/_Citadel/Contracts/Systems/SharedContractSystem.cs
+++ Content.Shared/_Citadel/Contracts/Systems/SharedContractSystem.cs
@@ -22,37 +22,37 @@ public abstract class SharedContractSystem : EntitySystem
         RaiseLocalEvent(ent, ref ev, broadcast: true);
     }
 
-    public bool TrySignContract(Entity<CitadelContractComponent> ent)
+    public TransitionError? TrySignContract(Entity<CitadelContractComponent> ent)
     {
         if (ent.Comp.State is not ContractStateUnsigned)
-            return false;
+            return new TECannotTransitionFrom(ent.Comp.State);
 
         if (ent.Comp.PartyA.Count == 0)
-            return false;
+            return new TENoPartyA();
 
         if (ent.Comp.PartyB.Count == 0)
-            return false;
+            return new TENoPartyB();
 
         Transition(ent, new ContractStateSigned());
-        return true;
+        return null;
     }
 
-    public bool TryBreachContract(Entity<CitadelContractComponent> ent, Party party)
+    public TransitionError? TryBreachContract(Entity<CitadelContractComponent> ent, Party party)
     {
         if (ent.Comp.State is not ContractStateSigned)
-            return false;
+            return new TECannotTransitionFrom(ent.Comp.State);
 
         Transition(ent, new ContractStateBreached { BreachingParty = party });
-        return true;
+        return null;
     }
 
-    public bool TryCloseOutContract(Entity<CitadelContractComponent> ent)
+    public TransitionError? TryCloseOutContract(Entity<CitadelContractComponent> ent)
     {
         if (ent.Comp.State is not ContractStateSigned)
-            return false;
+            return new TECannotTransitionFrom(ent.Comp.State);
 
         Transition(ent, new ContractStateClosedOut());
-        return true;
+        return null;
     }
 
     /// <remarks>
@@ -85,5 +85,12 @@ public enum TransitionStatus
 {
     Invalid = 0,
     Success,
-
+    CannotTransitionFrom,
+    NoPartyA,
+    NoPartyB,
 }
+
+public abstract record TransitionError();
+public sealed record TENoPartyA : TransitionError;
+public sealed record TENoPartyB : TransitionError;
+public sealed record TECannotTransitionFrom(ContractState State) : TransitionError;
