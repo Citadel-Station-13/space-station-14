651c683b0ff833fc3d623e5e825362e8cd07ffdb
lunar-crater-ex <112364074+lunar-crater-ex@users.noreply.github.com>
2024-07-04 19:08:51 -0500

    Update actions for our default branch.

--- Tools/dump_github_contributors.ps1
+++ Tools/dump_github_contributors.ps1
@@ -113,7 +113,7 @@ function load_contribs([string] $repo)
 }
 
 $engineJson = load_contribs("space-wizards/RobustToolbox")
-$contentJson = load_contribs("space-wizards/space-station-14")
+$contentJson = load_contribs("Citadel-Station-13/space-station-14")
 
 ($engineJson).login + ($contentJson).login + ($add) `
     | select -unique `
