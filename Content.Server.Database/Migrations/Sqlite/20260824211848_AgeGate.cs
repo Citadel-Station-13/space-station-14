6dbab7fec7566a043add4bb144236c4ba7359ae5
0x6273 <0x40@keemail.me>
2026-08-24 23:21:11 +0200

    Run EF Core migrations

---
+++ Content.Server.Database/Migrations/Sqlite/20260824211848_AgeGate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Sqlite
+{
+    /// <inheritdoc />
+    public partial class AgeGate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "has_passed_age_gate",
+                table: "player",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "has_passed_age_gate",
+                table: "player");
+        }
+    }
+}
